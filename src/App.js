074094fa3df397ccaa32fcd5b0288710899ee528
hjbuchanan <hannahbuchanan4@gmail.com>
2020-02-18 08:32:55 -0500

    write basic tests checking that button display the correct text

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ class App extends Component {
   render() {
     return (
       <div className="App mt3">
-        <button onClick={this.displayText}>Click Me!</button>
+        <button onClick={this.displayText} id="main-button">Click Me!</button>
         <div>
-          { this.state.displayText ? <p>This button works.</p> :null }
+          { this.state.displayText ? <p className="displayText" id="displayText">This button works.</p> :null }
         </div>
       </div>
     );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
+
 import App from './App';
+
+let container;
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('displays the button', () => {
+    act(() => {
+      ReactDOM.render(<App />, container);
+  	});
+  	const button = container.querySelector('button');
+  	expect(button.textContent).toBe('Click Me!');
+});
+
+it('shows the text when the button is clicked', () => {
+	act(() => {
+      ReactDOM.render(<App />, container);
+  	});
+	 
+	const button = container.querySelector('button');
+
+    act(() => {
+      button.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+ 	});
+  	const text = container.querySelector('p');
+  	expect(text.textContent).toBe('This button works.');
 });
